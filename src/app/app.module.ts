1d2e5de541aa3d0c7afa86734279c9e8a6070e4a
AbhinavChandran98 <abhinavchandran0@gmail.com>
2022-08-03 23:27:18 +0530

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,36 @@ import { AppComponent } from './app.component';
 import { AddProductsComponent } from './add-products/add-products.component';
 import { SearchProductComponent } from './search-product/search-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+  {
+    path:"",
+    component:ViewProductComponent
+  },
+  {
+    path:"Add",
+    component:AddProductsComponent
+  },
+  {
+    path:"Search",
+    component:SearchProductComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddProductsComponent,
     SearchProductComponent,
-    ViewProductComponent
+    ViewProductComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
